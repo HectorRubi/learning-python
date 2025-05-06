85f1124a2c3c0169dd1532a57fd0be79afcde015
Hector Rubi <hector.rubi.garcia@outlook.com>
2025-05-05 19:31:49 -0600

    Add variables and data types examples to the basics module

---
+++ basics/variables_and_data_types.py
@@ -0,0 +1,41 @@
+# Variables and Data Types
+
+a = 'string'
+print(type(a))  # <class 'str'>
+print(a)  # string
+
+a = 1
+print(type(a))  # <class 'int'>
+print(a)  # 1
+
+a = 1.0
+print(type(a))  # <class 'float'>
+print(a)  # 1.0
+
+a = True
+print(type(a))  # <class 'bool'>
+print(a)  # True
+
+a = None
+print(type(a))  # <class 'NoneType'>
+print(a)  # None
+
+# List
+a = [1, 2, 3]
+print(type(a))  # <class 'list'>
+print(a)  # [1, 2, 3]
+
+# Tuple
+a = (1, 2, 3)
+print(type(a))  # <class 'tuple'>
+print(a)  # (1, 2, 3)
+
+# Set
+a = {1, 2, 3}
+print(type(a))  # <class 'set'>
+print(a)  # {1, 2, 3}
+
+# Dictionary
+a = {'key': 'value'}
+print(type(a))  # <class 'dict'>
+print(a)  # {'key': 'value'}
