fbf4cbbf37a366689fde12f4a37d270af9cd16b9
Hector Rubi <hector.rubi.garcia@outlook.com>
2025-05-05 19:54:35 -0600

    Add math operators examples to the basics module

---
+++ basics/math_operators.py
@@ -0,0 +1,54 @@
+# Addition
+print(2 + 3)  # Output: 5
+
+# Subtraction
+print(5 - 2)  # Output: 3
+
+# Multiplication
+print(2 * 3)  # Output: 6
+
+# Division
+print(7 / 2)  # Output: 3.0
+
+# Division entera
+# Floor Division
+print(7 // 2)  # Output: 3
+
+# Modulus
+print(7 % 2)  # Output: 1
+
+# Exponentiation
+print(2 ** 3)  # Output: 8
+
+# PEMDAS
+# P. Parentheses
+# E. Exponents,
+# MD. Multiplication and Division (from left to right), 
+# AS. Addition and Subtraction (from left to right)
+
+# 2 + (3 * 4) = 2 + 12 = 14
+print(2 + 3 * 4)  # Output: 14
+
+# 2 * (3 + 4) = 2 * 7 = 14
+print(2 * (3 + 4))  # Output: 14
+
+# 2 + 3 * 4 - 5 = 2 + 12 - 5 = 9
+print(2 + 3 * 4 - 5)  # Output: 9
+
+# 2 + 3 * 4 / 2 = 2 + 12 / 2 = 2 + 6 = 8
+print(2 + 3 * 4 / 2)  # Output: 8
+
+# 2 + 3 * 4 // 2 = 2 + 12 // 2 = 2 + 6 = 8
+print(2 + 3 * 4 // 2)  # Output: 8
+
+# 2 + 3 * 4 % 2 = 2 + 12 % 2 = 2 + 0 = 2
+print(2 + 3 * 4 % 2)  # Output: 2
+
+# 2 + 3 * 4 ** 2 = 2 + 3 * 16 = 2 + 48 = 50
+print(2 + 3 * 4 ** 2)  # Output: 50
+
+# 2 + 3 * 4 ** 2 // 2 = 2 + 48 // 2 = 2 + 24 = 26
+print(2 + 3 * 4 ** 2 // 2)  # Output: 26
+
+# 2 + 3 * 4 ** 2 % 2 = 2 + 48 % 2 = 2 + 0 = 2
+print(2 + 3 * 4 ** 2 % 2)  # Output: 2
