d016acab9c239395d40049cb2448605c91fc5386
Hector Rubi <hector.rubi.garcia@outlook.com>
2025-05-05 19:46:35 -0600

    Add print function examples to the basics module

---
+++ basics/print_function.py
@@ -0,0 +1,32 @@
+# print function
+
+# prints "Hello World!" to the console
+print("Hello World!")
+
+# prints "Hello World!" to the console with "!" at the end
+print("Hello World!", end="!")
+
+# prints "Hello, World!" to the console with "!" at the end and a newline
+print("Hello World!", end="!\n")
+
+# prints "Hello, World!" using separators
+print("Hello", "World", sep=", ")
+
+# prints the text variable to the console
+text = "Hello World!"
+print("Text:", text)
+
+# f-string formatting
+print(f"Text: {text}")
+
+# format method
+text1 = "Hello"
+text2 = "World"
+print("Text1: {} Text2: {}".format(text1, text2))
+
+# format 2 decimal places
+pi = 3.14959
+print("PI: {:.2f}".format(pi))
+
+# prints "Hello World!" to the console with a newline between the words
+print("Hola\nMundo")
